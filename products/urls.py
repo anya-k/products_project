445a2d485afebaf263b49ae479297e9efff6240c
Anna Kostenko <blackqueennn@gmail.com>
2016-02-13 21:27:10 +0200

    Add list of new product

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from products import views
 
 urlpatterns = [
     url(r'^$', views.CategoryListView.as_view(), name='category_list'),
+    url(r'^new/$', views.LastAddProductListView.as_view(), name='product_new'),
     url(r'^(?P<slug>[\w-]+)/$', views.ProductListView.as_view(), name='product_list'),
-    url(r'^(?P<slug2>[\w-]+)/(?P<slug>[\w-]+)/$', views.ProductDetailView.as_view(), name='product_detail'),
+    url(r'^(?P<slug_category>[\w-]+)/(?P<slug>[\w-]+)/$', views.ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
 from django.shortcuts import render
+import datetime
 
 from models import Category, Product
 
@@ -23,8 +24,44 @@ class ProductListView(ListView):
             self.queryset = Product.objects.filter(category_id=category[0].id)
         return super(ProductListView, self).get(request, *args, **kwargs)
 
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        category_slug = self.kwargs.get('slug')
+        category = Category.objects.filter(slug=category_slug)
+        if category:
+            context['category'] = category[0]
+        return context
+
 
 class ProductDetailView(DetailView):
     model = Product
     query_pk_and_slug = True
     template_name = 'products/product_detail.html'
+
+
+hours_delta = 24
+
+
+class LastAddProductListView(ListView):
+    model = Product
+    template_name = 'products/new_product_list.html'
+
+    def get(self, request, *args, **kwargs):
+        date_from = datetime.datetime.now() - datetime.timedelta(hours=hours_delta)
+        new_products = Product.objects.filter(created_at__gte=date_from)
+        self.queryset = new_products
+        return super(LastAddProductListView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(LastAddProductListView, self).get_context_data(**kwargs)
+        category_list = {}
+        for product in self.object_list:
+            name = product.category
+            if name not in category_list:
+                category_list.update({name: [product, ]})
+            else:
+                category_list[name].append(product)
+
+        context['category_list'] = category_list
+        return context
+
