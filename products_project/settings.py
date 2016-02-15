b9f5cc305e3c39d204449bd791415d6d76440f52
Anna Kostenko <blackqueennn@gmail.com>
2016-02-15 22:09:56 +0200

    Settings for production

--- products_project/settings.py
+++ products_project/settings.py
@@ -136,7 +136,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 )
 
 LOGOUT_URL = '/'
---
+++ products_project/settings_production.py
@@ -0,0 +1,19 @@
+from settings import *
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = '/home/cadet/help_static'
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_BROWSER_XSS_FILTER = True
+# SECURE_HSTS_SECONDS = 3600
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
